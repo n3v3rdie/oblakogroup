0b5d212c04fcfca7589be8837576172053700398
Nikolay <n3v3r_die@mail.ru>
2021-06-04 09:06:23 +0300

    added render json

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,17 +3,19 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def create
-    if params['project_id']
+    if params['project_id'] and params['project_id'] != ''
       @project = Project.find(params['project_id'])
     else
       @project = Project.create(title: params['title'])
     end      
-    Todo.create(text: params['text'], isCompleted: params['isCompleted'], project: @project)
+    @todo = Todo.create(text: params['text'], isCompleted: params['isCompleted'], project: @project)
+    render json: @todo, :except => [:created_at, :updated_at]
   end
 
   def update
     @todo = Todo.find(params[:id])
     @todo.update(isCompleted: params['isCompleted'])
+    render json: @todo, :except => [:created_at, :updated_at]
   end
 
 end
