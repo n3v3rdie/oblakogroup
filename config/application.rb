4db25433dfe5a8083b1a4fe2aedfd19296cc8a5a
Nikolay <n3v3r_die@mail.ru>
2021-06-04 10:49:32 +0300

    added rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,18 @@ Bundler.require(*Rails.groups)
 module Mytask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+   
     config.load_defaults 6.1
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+
+        resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
 
     # Configuration for the application, engines, and railties goes here.
     #
