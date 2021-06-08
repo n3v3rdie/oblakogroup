008a34636a1046281ba62b56209c01fa672f75e4
Nikolay <n3v3r_die@mail.ru>
2021-06-08 09:28:39 +0300

    added validation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,19 +3,32 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def create
-    if params['project_id'] and params['project_id'] != ''
-      @project = Project.find(params['project_id'])
-    else
-      @project = Project.create(title: params['title'])
-    end      
+    @project = Project.find_or_create_by(id: params['project_id'], title: params['title'])
+    if @project.invalid?
+      render json: @project.errors, status: :unprocessable_entity
+      return
+    end
+      
     @todo = Todo.create(text: params['text'], isCompleted: params['isCompleted'], project: @project)
-    render json: @todo, :except => [:created_at, :updated_at]
+    if @todo.save
+#      render json: @todo, :except => [:created_at, :updated_at]
+#      render json: @project, :include => {:todos => {:except => [:created_at, :updated_at]}}, :except => [:created_at, :updated_at]
+      render json: @todo, :include => [:project => {:except => [:created_at, :updated_at]} ], :except => [:created_at, :updated_at]
+
+      return
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+      return
+    end
   end
 
   def update
     @todo = Todo.find(params[:id])
-    @todo.update(isCompleted: params['isCompleted'])
-    render json: @todo, :except => [:created_at, :updated_at]
+    if @todo.update(isCompleted: params['isCompleted'])
+      render json: @todo, :except => [:created_at, :updated_at]
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
   end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   has_many :todos
+  validates :title, presence: true
 end
